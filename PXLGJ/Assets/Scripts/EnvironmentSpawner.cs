bd9289f1e27e3c062bee9c9eb6013d1b8eefd820
elijahnbryce <125401109+elijahnbryce@users.noreply.github.com>
2024-05-18 03:29:47 -0400

    Enemy Behavior (no Moving)
    ~ Switching Sprite of enemy
    ~ ? Env Spawner Maybe
    
    ~ Tiles too big
       ~.! Tilemap / palette not yet setup

--- PXLGJ/Assets/Scripts/EnvironmentSpawner.cs
+++ PXLGJ/Assets/Scripts/EnvironmentSpawner.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class EnvironmentSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int height, width, objSize = 3;
+    [SerializeField] private Transform spawningParent;
+    [SerializeField] private float spawnChance = 0.13f, spawnRange = 27f;
+    [SerializeField] private List<GameObject> obstacles;
+    [SerializeField] private GameObject objParent;
+    // [SerializeField] private GameObject listObj;
+
+    private void Start()
     {
-        
+        //TileSpawning();
+        AreaSpawning();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void TileSpawning()
     {
-        
+        for (int i = height / objSize; i > 0; i--)
+        {
+            for (int j = 0; j < width / objSize; j++)
+            {
+                int x = j * objSize;
+                int y = i * objSize;
+                /* For Isometric Tiles */
+                    x = (x - y) / 2;
+                    y - (y - x) / 4; 
+                if (Random.Range(0, 1) < spawnChance) 
+                {
+                    int index = Random.Range(0, obstacles.Count);
+                    Transform spwntrans = new Vector3(x , y, 1);
+                    Instantiate(obstacles[index], spwntrans, Quaternion.Identity);
+                }
+            }
+        }
+    }
+
+    private void AreaSpawning()
+    {
+        height = spawningParent.scale.y / 2;
+        width = spawningParent.scale.x / 2;
+
+        for (int i = 0; i < height / objSize; i++)
+        {
+            for (int j = 0; j < width / objSize; j++)
+            {
+                int x = j * objSize;
+                int y = i * objSize;
+                for (int k = 0; k < 2; k++)
+                {
+                    if (Random.Range(0, 1) < spawnChance) 
+                    {
+                        int index = Random.Range(0, obstacles.Count);
+                        Transform spwntrans = new Vector3(spawningParent.position.x - (x * ((k * 2) - 1)) , spawningParent.position.y - (y * ((k * 2) - 1)), 1);
+                        Instantiate(obstacles[index], spwntrans, Quaternion.Identity);
+                    }
+                }
+            }
+        }
     }
 }
--- PXLGJ/Assets/Scripts/NarcoBehavior.cs
+++ PXLGJ/Assets/Scripts/NarcoBehavior.cs
@@ -4,15 +4,89 @@ using UnityEngine;
 
 public class NarcoBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")]
+    public enum EnemyState
     {
-        
+        running,
+        caught
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private EnemyState state;
+    [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private Transform escapeListParent;
+    private Transform closestEscape;
+    private Vector2 targDir;
+
+    [Header("Sprite")]
+    [SerializeField] private float timeHidden = 7f, timeShown = 7f, bubbleTime = 3f;
+    [SerializeField] private float[] statuses = [timeHidden, bubbleTime, timeShown];
+    private string[] statusAnims = ["Hide", "Bubble", "Show"];
+    private Animator anim;
+
+    public float statusTime;
+    public int statusIndex;
+
+    private void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        FindClosestEscape();
+
+    }
+    private void Update()
+    {
+        switch (state)
+        {
+            case EnemyState.running:
+                MoveEnemy();
+                RotateStatus();
+                break;
+            
+            case EnemyState.caught:
+                break;
+        }
+    }
+
+    private void FindClosestEscape()
+    {
+        float nearestDistance;
+        foreach (Transform child in escapeListParent)
+        {
+            Vector2 dir2 = child.position - transform.position;
+            float distance2 = dir2.magnitude;
+            if (distance2 < nearestDistance or nearestDistance == 0)
+            {
+                closestEscape = child;
+                nearestDistance = distance2;
+                targDir = dir2;
+            }
+        }
+    }
+
+    private void MoveEnemy()
+    {
+        // Navmesh probablby
+        // move twrds closestEscape
+    }
+
+
+    private void RotateStatus()
+    {
+        statusTime -= Time.deltaTime;
+        if (statusTime <= 0)
+        {
+            statusIndex = (statusIndex + 1) % statuses.Length;
+            StatusSwitchSprite();
+        }
+    }
+
+    private void StatusSwitchSprite()
+    {
+        statusTime = statuses[statusIndex];
+        anim.Play(statusAnims[statusIndex]);
+    }
+
+    private IEnumerator HideShowNarc()
+    {
+        yield return new WaitForSeconds(1f);
     }
 }
